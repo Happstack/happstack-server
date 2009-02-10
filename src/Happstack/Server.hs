0fb41960670d41bbb6bb71afe53e5bf939ab14d4
Matthew Elder <matt@mattelder.org>
2009-02-10 05:40:00 +0000

    fixed broken build (whoops!)

--- src/Happstack/Server.hs
+++ src/Happstack/Server.hs
@@ -1,14 +1,11 @@
 module Happstack.Server 
-(
-,module Happstack.Server.XSLT
+(module Happstack.Server.XSLT
 ,module Happstack.Server.SimpleHTTP
 ,module Happstack.Server.HTTP.Client
 ,module Happstack.Server.MessageWrap
 ,module Happstack.Server.HTTP.FileServe
 ,module Happstack.Server.StdConfig
-,module Happstack.Store.Util
-
-)
+,module Happstack.Store.Util)
 where
 import Happstack.Server.HTTP.Client
 import Happstack.Server.StdConfig
