9e46a094463df8251f11b70b64aaaed9b36c1502
jeremy <jeremy@n-heptane.com>
2009-11-14 17:11:17 +0000

    happstack-server: fix supportsIPv6 to work with newer versions of network library Show resolve this 'Unsupported socket' error. http://code.google.com/p/happstack/issues/detail?id=88

--- src/Happstack/Server/HTTP/SocketTH.hs
+++ src/Happstack/Server/HTTP/SocketTH.hs
@@ -8,9 +8,8 @@ import Network.Socket(SockAddr(..))
 
 -- find out at compile time if the SockAddr6 / HostAddress6 constructors are available
 supportsIPv6 :: Bool
-supportsIPv6 = $(let c = "Network.Socket.SockAddrInet6"; d = ''SockAddr in
+supportsIPv6 = $(let c = ["Network.Socket.SockAddrInet6", "Network.Socket.Internal.SockAddrInet6"] ; d = ''SockAddr in
                  do TyConI (DataD _ _ _ cs _) <- reify d
-                    if isJust (find (\(NormalC n _) -> show n == c) cs)
+                    if isJust (find (\(NormalC n _) -> show n `elem` c) cs)
                        then [| True |]
                        else [| False |] )
-                       
