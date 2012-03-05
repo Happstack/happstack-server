dc78333b7902746c2762b88833d851bfda24b143
jeremy <jeremy@n-heptane.com>
2012-03-05 16:32:16 +0000

    happstack-server: fix compiler warning in supportsIPv6

--- src/Happstack/Server/Internal/SocketTH.hs
+++ src/Happstack/Server/Internal/SocketTH.hs
@@ -2,14 +2,21 @@
 module Happstack.Server.Internal.SocketTH(supportsIPv6) where
 import Language.Haskell.TH
 
-import Data.List
-import Data.Maybe
 import Network.Socket(SockAddr(..))
 
 -- find out at compile time if the SockAddr6 / HostAddress6 constructors are available
 supportsIPv6 :: Bool
-supportsIPv6 = $(let c = ["Network.Socket.SockAddrInet6", "Network.Socket.Internal.SockAddrInet6"] ; d = ''SockAddr in
-                 do TyConI (DataD _ _ _ cs _) <- reify d
-                    if isJust (find (\(NormalC n _) -> show n `elem` c) cs)
-                       then [| True |]
-                       else [| False |] )
+supportsIPv6 = $(let c = ["Network.Socket.SockAddrInet6", "Network.Socket.Internal.SockAddrInet6"] ; d = ''SockAddr
+                     isInet6 :: Con -> Bool
+                     isInet6 (NormalC n _) = show n `elem` c
+                     isInet6 _             = False
+                 in
+                 do info <- reify d
+                    case info of
+                      TyConI (DataD _ _ _ cs _) ->
+                        if any isInet6 cs
+                          then [| True |]
+                          else [| False |]
+                      _ -> error "supportsIPv6: SockAddr is no longer a TyConI ?!?! Giving up."
+                )
+
