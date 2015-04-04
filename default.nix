1b5f5b9df3a0b325ff7be7aa8be6f226a4ed5a5a
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-04 17:52:14 -0500

    7.4.3 - bumped version. added default.nix.

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ mkDerivation, base, base64-bytestring, blaze-html, bytestring
+, containers, directory, exceptions, extensible-exceptions
+, filepath, hslogger, html, HUnit, monad-control, mtl, network
+, network-uri, old-locale, parsec, process, sendfile, stdenv, syb
+, system-filepath, template-haskell, text, threads, time
+, time-compat, transformers, transformers-base, transformers-compat
+, unix, utf8-string, xhtml, zlib
+}:
+mkDerivation {
+  pname = "happstack-server";
+  version = "7.4.3";
+  src = ./.;
+  buildDepends = [
+    base base64-bytestring blaze-html bytestring containers directory
+    exceptions extensible-exceptions filepath hslogger html
+    monad-control mtl network network-uri old-locale parsec process
+    sendfile syb system-filepath template-haskell text threads time
+    time-compat transformers transformers-base transformers-compat unix
+    utf8-string xhtml zlib
+  ];
+  testDepends = [ base bytestring containers HUnit parsec zlib ];
+  homepage = "http://happstack.com";
+  description = "Web related tools and services";
+  license = stdenv.lib.licenses.bsd3;
+}
